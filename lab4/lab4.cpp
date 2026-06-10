379c8ee1f1da767cc10c65b8c927c66e18b00865
ddunmerr <55109658+ddunmerr@users.noreply.github.com>
2026-06-10 18:28:16 +0300

    Update lab4.cpp

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,3 +1,4 @@
+#include "CShapeController.h"
 #include "CShapeFinder.h"
 #include "CShapeParser.h"
 #include "consts.h"
@@ -7,44 +8,6 @@
 
 int main()
 {
-	std::vector<std::unique_ptr<IShape>> shapes;
-	std::string line;
-
-	while (std::getline(std::cin, line))
-	{
-		if (line.empty())
-		{
-			continue;
-		}
-
-		auto shape = CShapeParser::ParseCommand(line);
-		if (shape)
-		{
-			shapes.push_back(std::move(shape));
-		}
-		else
-		{
-			std::cout << ERROR_UNKNOWN_COMMAND << line << std::endl;
-		}
-	}
-
-	if (!shapes.empty())
-	{
-		CShapeFinder finder(shapes);
-
-		IShape* maxAreaShape = finder.FindShapeWithMaxArea();
-		IShape* minPerimeterShape = finder.FindShapeWithMinPerimeter();
-
-		std::cout << SHAPE_MAX_AREA << std::endl;
-		std::cout << maxAreaShape->ToString() << std::endl;
-
-		std::cout << SHAPE_MIN_AREA << std::endl;
-		std::cout << minPerimeterShape->ToString() << std::endl;
-	}
-	else
-	{
-		std::cout << ERROR_NO_SHAPES << std::endl;
-	}
-
-	return 0;
+	CShapeController controller(std::cin, std::cout);
+	controller.Run();
 }
